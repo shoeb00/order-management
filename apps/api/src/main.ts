f1af4ecfaf1b329050ea45a019c3d5bf130ea91c
shoeb00 <syedshoeb886@gmail.com>
2025-11-25 18:55:18 +0530

    init

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,19 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Order Management System')
+    .setDescription('The Order Management System API description')
+    .setVersion('1.0')
+    .addTag('e-commerce')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(3000);
 }
 
