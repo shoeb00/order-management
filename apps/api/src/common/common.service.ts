fbce835ae1e74aa5705c68e91596a28c4b9c92c0
shoeb00 <syedshoeb886@gmail.com>
2025-11-26 17:57:30 +0530

    feat: adding common module and services for id and coupon generation logic

---
+++ apps/api/src/common/common.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CommonService {
+  getUID(length: number = 6): number {
+    const min = Math.pow(10, length - 1);
+    const max = Math.pow(10, length) - 1;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  generateRandomString(length: number = 6): string {
+    let result = '';
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+}
