7580b2cbbe167fe65c2a16cf51f67358868498c1
shoeb00 <syedshoeb886@gmail.com>
2025-11-26 18:14:12 +0530

    feat: updating database with all now variables and interfaces

--- apps/api/src/database/interfaces/database.interface.ts
+++ apps/api/src/database/interfaces/database.interface.ts
@@ -1,10 +1,13 @@
 import { Cart } from 'src/cart/interfaces/cart.interface';
+import { Coupon } from 'src/coupon/interfaces/coupon.interface';
 import { Order } from 'src/order/interfaces/order.interface';
 import { Product } from 'src/product/interfaces/product.interface';
 
 export interface Database {
   products: Product[];
   orders: Order[];
-  coupons: any[];
+  coupons: Coupon[];
   carts: Cart[];
+  couponCycle: number;
+  discountPercentage: number;
 }
