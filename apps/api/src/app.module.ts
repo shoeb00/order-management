fbe939dde99a7d5a58a471f59c84717aec401068
shoeb00 <syedshoeb886@gmail.com>
2025-11-26 17:59:06 +0530

    feat: importing all modules in app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,23 @@
 import { Module } from '@nestjs/common';
 
-import { LinksModule } from './links/links.module';
-
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { ProductModule } from './product/product.module';
+import { DatabaseModule } from './database/database.module';
+import { OrderModule } from './order/order.module';
+import { CouponModule } from './coupon/coupon.module';
+import { CartModule } from './cart/cart.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [LinksModule],
+  imports: [
+    ProductModule,
+    DatabaseModule,
+    OrderModule,
+    CouponModule,
+    CartModule,
+    CommonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
