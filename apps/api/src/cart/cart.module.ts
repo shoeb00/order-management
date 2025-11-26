93302856619ccda677b00efb48474ee3d818d69a
shoeb00 <syedshoeb886@gmail.com>
2025-11-26 18:13:36 +0530

    feat: updating checkout service for cart to create order for the user with coupon

--- apps/api/src/cart/cart.module.ts
+++ apps/api/src/cart/cart.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { CartController } from './cart.controller';
 import { CartService } from './cart.service';
 import { DatabaseModule } from 'src/database/database.module';
+import { CommonModule } from 'src/common/common.module';
+import { OrderModule } from 'src/order/order.module';
+import { CouponModule } from 'src/coupon/coupon.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CommonModule, OrderModule, CouponModule],
   controllers: [CartController],
   providers: [CartService],
 })
